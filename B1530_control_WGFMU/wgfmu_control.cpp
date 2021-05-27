add79ee165d7cdf4350b6e6fdaa3b80e8a37bc2b
Pierre-Antoine Mouny <47055772+PAMouny@users.noreply.github.com>
2021-05-27 15:48:26 -0400

    Adding log convergence for multilevel programming
    Developping a new multilevel programming algorithm for faster convergence to resistance state. Debugged and built. Testing need to be done on devices.

--- B1530_control_WGFMU/wgfmu_control.cpp
+++ B1530_control_WGFMU/wgfmu_control.cpp
@@ -11,9 +11,10 @@
 #include <Windows.h>
 #include "wgfmu.h"
 #include "wgfmu_control.h"
+#include <math.h>
 
 
-int main(){
+int main() {
     //TO DO ==> AUTOMATIC ACQUISITION OF HRS AND LRS VALUES
     double LRS;
     printf("Enter LRS value: ");
@@ -23,69 +24,36 @@ int main(){
     printf("Enter HRS value: ");
     scanf("%lf", &HRS);
     printf("Selected LRS is %f \n", HRS);
-    double step_size;
-    printf("Enter step for pulse amplitude: ");
-    scanf("%lf", &step_size);
-    double V_LTP;
-    printf("Enter V_LTP: ");
-    scanf("%lf", &V_LTP);
-    double V_LTD;
-    printf("Enter V_LTD: ");
-    scanf("%lf", &V_LTD);
-    while(1){
-        /*int nb_state;
-        printf("Enter number of resistance levels: ");
-        scanf("%d", &nb_state);*/
-        double list_resist[nb_state];
-        for(int i=0;i<nb_state;i++){
-            list_resist[i] = LRS + i*(HRS-LRS)/(nb_state-1);
-        }
-        //Multilevel programming for all resistance states
-        for(int i_state=0; i_state<nb_state; i_state++){
-            double R_target = list_resist[i_state];
-            printf("Target resist is %f \n", R_target);
-            double Rmax = R_target*(1+tolerance);
-            double Rmin = R_target*(1-tolerance);
-            double Vp = V_LTP;
-            double Vn = V_LTD;
-            int c = 0;
-            int pulse_number = 0;
-            double pulse_amp = 0;
-            double R_c = read_resistance(Vr, t_read, topChannel, bottomChannel, pulse_amp);//put input there
-            while(c<stop){
-                if(Rmin<=R_c && R_c<=Rmax){
-                    Vp = V_LTP;
-                    Vn = V_LTD;
-                    printf("Reached target resistance \n");
-                    pulse_amp = 0;
-                    c++;
-                }
-                else if(R_c>Rmax){
-                    write_resistance(Vp, t_pulse, topChannel, bottomChannel);//put input there
-                    if (Vp < V_max) {
-                        Vp = Vp + step_size;
-                    }
-                    Vn=V_LTD;
-                    pulse_amp = Vp;
-                    printf("Apply positive pulse %f \n", Vp);
-                    //c=0; //Could cause issue
-                }
-                else if(R_c<Rmin){
-                    write_resistance(Vn, t_pulse, topChannel, bottomChannel);//put input
-                    if (Vn > -V_max) {
-                        Vn = Vn - step_size;
-                    }
-                    Vp=V_LTP;
-                    pulse_amp = Vn;
-                    printf("Apply negative pulse %f \n", Vn);
-                    //c=0; //Could cause issue
-                }
-                pulse_number++;
-                R_c = read_resistance(Vr, t_read, topChannel, bottomChannel, pulse_amp);//put input
-                printf("Measured resistance is %f \n", R_c);
-            } 
-        }      
+    int choice;
+    printf("Which measurement? (simple = 1, log = 2) \n");
+    scanf("%d", &choice);;
+    if (choice == 1) {
+        printf("Now executing simple multilevel programming of %d resistance states \n", nb_state);
+        double step_size;
+        printf("Enter step for pulse amplitude: ");
+        scanf("%lf", &step_size);
+        double V_LTP;
+        printf("Enter V_LTP: ");
+        scanf("%lf", &V_LTP);
+        double V_LTD;
+        printf("Enter V_LTD: ");
+        scanf("%lf", &V_LTD);
+        simple_convergence(LRS, HRS, V_LTD, V_LTP, step_size);
     }
+    else if (choice == 2) {
+        printf("Now executing logarithmic multilevel programming of %d resistance states \n", nb_state);
+        double V_LTP;
+        printf("Enter V_LTP: ");
+        scanf("%lf", &V_LTP);
+        double V_LTD;
+        printf("Enter V_LTD: ");
+        scanf("%lf", &V_LTD);
+        log_convergence(LRS, HRS, V_LTD, V_LTP);
+    }
+    else {
+        printf("Enter a defined operation type! \n You entered %d", choice);
+    }
+
     return 0;
 }
 
@@ -190,7 +158,136 @@ double extract_results(int channelId1, int channelId2, int offset,  const char* 
     fclose(fp);
 }
 
-//TO DO add limits for max amplitude
+void simple_convergence(double LRS, double HRS, double V_LTD, double V_LTP, double step_size)
+{
+    /*int nb_state;
+printf("Enter number of resistance levels: ");
+scanf("%d", &nb_state);*/
+    double list_resist[nb_state];
+    for (int i = 0;i < nb_state;i++) {
+        list_resist[i] = LRS + i * (HRS - LRS) / (nb_state - 1);
+    }
+    //Multilevel programming for all resistance states
+    for (int i_state = 0; i_state < nb_state; i_state++) {
+        double R_target = list_resist[i_state];
+        printf("Target resist is %f \n", R_target);
+        double Rmax = R_target * (1 + tolerance);
+        double Rmin = R_target * (1 - tolerance);
+        double Vp = V_LTP;
+        double Vn = V_LTD;
+        int c = 0;
+        int pulse_number = 0;
+        double pulse_amp = 0;
+        double R_c = read_resistance(Vr, t_read, topChannel, bottomChannel, pulse_amp);//put input there
+        while (c < stop) {
+            if (Rmin <= R_c && R_c <= Rmax) {
+                Vp = V_LTP;
+                Vn = V_LTD;
+                printf("Reached target resistance \n");
+                pulse_amp = 0;
+                c++;
+            }
+            else if (R_c > Rmax) {
+                write_resistance(Vp, t_pulse, topChannel, bottomChannel);//put input there
+                if (Vp < V_max) {
+                    Vp = Vp + step_size;
+                }
+                Vn = V_LTD;
+                pulse_amp = Vp;
+                printf("Apply positive pulse %f \n", Vp);
+                //c=0; //Could cause issue
+            }
+            else if (R_c < Rmin) {
+                write_resistance(Vn, t_pulse, topChannel, bottomChannel);//put input
+                if (Vn > -V_max) {
+                    Vn = Vn - step_size;
+                }
+                Vp = V_LTP;
+                pulse_amp = Vn;
+                printf("Apply negative pulse %f \n", Vn);
+                //c=0; //Could cause issue
+            }
+            pulse_number++;
+            R_c = read_resistance(Vr, t_read, topChannel, bottomChannel, pulse_amp);//put input
+            printf("Measured resistance is %f \n", R_c);
+        }
+    }
+    printf("End of simple multilevel programming");
+}
+
+void log_convergence(double LRS, double HRS, double V_LTD, double V_LTP)//Logarithmic multilevel programming
+{
+    /*int nb_state;
+printf("Enter number of resistance levels: ");
+scanf("%d", &nb_state);*/
+    double list_resist[nb_state];
+    for (int i = 0;i < nb_state;i++) {
+        list_resist[i] = LRS + i * (HRS - LRS) / (nb_state - 1);
+    }
+    //Multilevel programming for all resistance states
+    for (int i_state = 0; i_state < nb_state; i_state++) {
+        double R_target = list_resist[i_state];
+        printf("Target resist is %f \n", R_target);
+        double Rmax = R_target * (1 + tolerance);
+        double Rmin = R_target * (1 - tolerance);
+        double Vp = V_LTP;
+        double Vn = V_LTD;
+        int c = 0;
+        int count_n = 0;
+        int count_p = 0;
+        int pulse_number = 0;
+        double pulse_amp = 0;
+        double R_shift = 0;
+        double R_c = read_resistance(Vr, t_read, topChannel, bottomChannel, pulse_amp);//put input there
+        while (c < stop) {
+            if (Rmin <= R_c && R_c <= Rmax) {
+                Vp = V_LTP;
+                Vn = V_LTD;
+                count_n = 0;
+                count_p = 0;
+                printf("Reached target resistance \n");
+                pulse_amp = 0;
+                c++;
+            }
+            else if (R_c > Rmax) {
+                if (count_p >= increment && R_shift < min_shift * (HRS - LRS)) {
+                    Vp = Vp+a*log10((R_c - R_target)/R_shift); //Maybe issues 
+                    count_p = 0;
+                }
+                else if(R_shift>max_shift*(HRS-LRS)){
+                    Vp = V_LTP;
+                }
+                write_resistance(Vp, t_pulse, topChannel, bottomChannel);
+                count_p++;
+                pulse_amp = Vp;
+                printf("Apply positive pulse %f \n", Vp);
+            }
+            else if (R_c < Rmin) {
+                if (count_n >= increment && R_shift < min_shift * (HRS - LRS)) {
+                    Vn = Vn - a * log10((R_target - R_c) / R_shift); //Maybe issues 
+                    count_n = 0;
+                }
+                else if (R_shift > max_shift * (HRS - LRS)) {
+                    Vn = V_LTD;
+                }
+                write_resistance(Vn, t_pulse, topChannel, bottomChannel);
+                count_n++;
+                pulse_amp = Vn;
+                printf("Apply negative pulse %f \n", Vn);
+            }
+            pulse_number++;
+            R_shift = R_c;
+            R_c = read_resistance(Vr, t_read, topChannel, bottomChannel, pulse_amp);
+            R_shift = R_shift - R_c;
+            if (R_shift < 0) {
+                R_shift = -R_shift;
+            }
+            printf("Measured resistance is %f \n", R_c);
+        }
+    }
+    printf("End of simple multilevel programming");
+}
+
 //Implement better convergence
 //Measurement on BB3
-//Need to save pulse amplitude as well
+//create timestamp
--- B1530_control_WGFMU/wgfmu_control.h
+++ B1530_control_WGFMU/wgfmu_control.h
@@ -5,6 +5,12 @@
 double read_resistance(double, double, int, int, double);
 void write_resistance(double, double, int, int);
 double extract_results(int, int, int, const char*, double);
+void simple_convergence(double, double, double, double, double);
+void log_convergence(double, double, double, double);
+static const double min_shift = 0.005; //used for log convergence less than a 0.5% shift with respect to HRS-LRS
+static const double max_shift = 0.2; //not more than a 20% shift with respect to HRS-LRS
+static const double a = 0.1; //Coefficient determining increase speed of pulse amplitude for log convergence
+static const int increment = 5; //Argument for log convergence
 static const double t_pulse = 2E-7; //Pulse width of writing pulses
 static const double Vr = 2E-1; //Amplitude of reading pulses
 static const double t_read = 1E-6; //Pulse width of reading pulses
